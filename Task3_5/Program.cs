62f3286e2723b47736650a86ff2a889e30bfd07e
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-03 15:46:41 +0300

    Готова задача 3_5

---
+++ Task3_5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task3_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            a = a % 10;
+            if (a == 0)
+            {
+                Console.WriteLine("Да");
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+            }
+        }
+    }
+}
