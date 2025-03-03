a92b62aece9d4c1b53412f5b3d88f2b62baa3eca
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-03 16:10:28 +0300

    Готова задача 3_7

---
+++ Task3_8/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task3_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());//
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
