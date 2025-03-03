3782e2853f3aad00cc85ef76473b5923d89c94f8
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-03 16:22:15 +0300

    Готова задача 3_9

---
+++ Task3_9/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task3_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());//Вводится число. Вывести «Да», если оно попадает в диапазон ( до -10, от 10), и «Нет» в противном случае.
+            if (!(-10 <= a && a <= 10))
+            {
+                Console.WriteLine("Да");
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+            }
+        }
+    }
+}
