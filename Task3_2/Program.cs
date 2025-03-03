14dadd66954d988b4ed7414899f6dddbce768f59
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-03 11:04:40 +0300

    готова задача 3_4

---
+++ Task3_2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+            int max = (firstNumber > secondNumber) ? firstNumber: secondNumber;
+            int max1 = (max > thirdNumber) ? max : thirdNumber;
+            Console.WriteLine(max1);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task3_4/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task3_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            int mediana;
+            if (a > b && b > c)
+            {
+                mediana = b;
+                Console.WriteLine("mediana= {0}", mediana);
+            }
+            if (a < b && b < c)
+            {
+                mediana = b;
+                Console.WriteLine("mediana= {0}", mediana);
+            }
+            if (a < b && b > c)
+            {
+                mediana = (a > c) ? a : c;
+                Console.WriteLine("mediana= {0}", mediana);
+            }
+            if (a > b && b < c)
+            {
+                mediana = (a < c) ? a : c;
+                Console.WriteLine("mediana= {0}", mediana);
+            }
+        }
+    }
+}
+
+
+
