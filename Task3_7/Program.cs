6e4cc3cd0bcebed58ade3d334c4d60d86ccf9747
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-03 16:04:57 +0300

    Готова задача 3_7

---
+++ Task3_7/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task3_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());//Вводится число. Вывести «Да», если оно двухзначное, и «Нет» в противном случае.
+            if (9 < a && a < 99)
+            {
+                Console.WriteLine("Да");
+            }
+            else
+            {
+                Console.WriteLine("Нет");
+            }
+        }
+    }
+}
