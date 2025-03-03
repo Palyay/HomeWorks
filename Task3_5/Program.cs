06774f3f3b9ce629c4fdbb0e7e8b80699ecdaba5
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-03 15:50:11 +0300

    Готова задача 3_5

--- Task3_5/Program.cs
+++ Task3_5/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int a = Convert.ToInt32(Console.ReadLine());
+            int a = Convert.ToInt32(Console.ReadLine());//Вводится число. Вывести «Да», если оно оканчивается на 0, и «Нет» в противном случае.
             a = a % 10;
             if (a == 0)
             {
