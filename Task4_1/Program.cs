916fc6c4a7dd08416b888e3296c4706547990143
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-05 16:40:25 +0300

    готова задача4_1

---
+++ Task4_1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1*2*3*4*...*n
+            int n = Convert.ToInt32(Console.ReadLine());
+            int s = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                s = i*s;
+            }
+            Console.WriteLine("Факториал {0} равен {1}", n, s);
+            Console.ReadKey();
+        }
+    }
+}
