b66b76bd1863081f00d441bf21ac1ad721247a94
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-06 11:13:56 +0300

    Сделал Task4_2

--- Task4_2/Program.cs
+++ Task4_2/Program.cs
@@ -8,13 +8,12 @@ namespace Task4_2
         {
             //1 + 1/2 + 1/3 + … + 1/n
             int n = Convert.ToInt32(Console.ReadLine());
-            double s =1;
-            for (int i = 1; i <= n; i++)
+            double sum = 1;
+            for (int i = 2; i <= n; i++)
             {
-               
-                s = s + (1.0 / (i + 1));
+                sum = sum + (1.0 / i);
             }
-            Console.WriteLine(" Сумма 1+1/2+...+1/{0} равна {1}", n, s);
+            Console.WriteLine(" Сумма 1+1/2+...+1/{0} равна {1}", n, sum);
             Console.ReadKey();
         }
     }
