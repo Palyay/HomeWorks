c0aa7360d0ac1d0d9040a451ba231427dbbeb9ad
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-06 12:39:07 +0300

    fix

--- Task4_4/Program.cs
+++ Task4_4/Program.cs
@@ -1,25 +1,20 @@
-﻿namespace Task4_3
+﻿namespace Task4_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //a>0, b>0. Найти a^b
             int a = Convert.ToInt32(Console.ReadLine());
             int b = Convert.ToInt32(Console.ReadLine());
             int st = a;
-
-            for (int i = 2; i <= b; i++)
+            if (a > 0) ;
+            if (b < 0) ;
             {
-                st = st * a;
-            
-
-            if (a <= 0) ;
-            if (b <= 0) ;
-            {
-                break;
-                Console.WriteLine(" Ошибка");
-            }}
+                for (int i = 2; i <= b; i++)
+                {
+                    st = st * a;
+                }
+            }
             Console.WriteLine(" {0} в степени {1} равено {2}", a, b, st);
             Console.ReadKey();
         }
