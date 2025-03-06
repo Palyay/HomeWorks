31e2ce8e19462c2bcca465108a802956da13815a
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-06 20:18:22 +0300

    Сделал Task4_4

--- Task4_4/Program.cs
+++ Task4_4/Program.cs
@@ -3,19 +3,22 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            int st = a;
-            if (a > 0) ;
-            if (b < 0) ;
+        {//a>0, b<0. Найти a^b.
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double st = a;
+            double st1 = a;
+            if (a > 0 && b < 0)
             {
-                for (int i = 2; i <= b; i++)
+                for (int i = 1; i <= -b; i++)
                 {
-                    st = st * a;
+                    st = 1.0 / (st1);
+                    st1 = st1 * a;
                 }
+                Console.WriteLine(" {0} в степени {1} равено {2}", a, b, st);
             }
-            Console.WriteLine(" {0} в степени {1} равено {2}", a, b, st);
+            else
+                Console.WriteLine("ошибка");
             Console.ReadKey();
         }
     }
