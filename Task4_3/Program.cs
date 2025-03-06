889b4f833417ed247465a162a345e485a5b677ae
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-06 19:33:42 +0300

    Сделал Task4_3

--- Task4_3/Program.cs
+++ Task4_3/Program.cs
@@ -4,23 +4,21 @@
     {
         static void Main(string[] args)
         {
-            //a>0, b>0. Найти a^b
+            //a>0, b>0. Найти a^b 
+            //не выходит на ошибке остановиться
             int a = Convert.ToInt32(Console.ReadLine());
             int b = Convert.ToInt32(Console.ReadLine());
             int st = a;
-
-            for (int i = 2; i <= b; i++)
+            if (a > 0 && b > 0)
             {
-                st = st * a;
-            
-
-            if (a <= 0) ;
-            if (b <= 0) ;
-            {
-                break;
-                Console.WriteLine(" Ошибка");
-            }}
-            Console.WriteLine(" {0} в степени {1} равено {2}", a, b, st);
+                for (int i = 2; i <= b; i++)
+                {
+                    st = st * a;
+                }
+                Console.WriteLine(" {0} в степени {1} равено {2}", a, b, st);
+            }
+            else
+            Console.WriteLine("ошибка");
             Console.ReadKey();
         }
     }
