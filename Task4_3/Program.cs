2705575010c2e006ec40d5723d2496595847676b
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-06 11:06:36 +0300

    Сделал Task4_3

---
+++ Task4_3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int st = a;
+            if (a > 0) ;
+            if (b > 0) ;
+            {
+                for (int i = 2; i <= b; i++)
+                {
+                    st = st * a;
+                }
+            }
+            Console.WriteLine(" {0} в степени {1} равено {2}", a, b, st);
+            Console.ReadKey();
+        }
+    }
+}
