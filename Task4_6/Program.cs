096f23ad9c301e46cd91d5cc95b1aa8c1e41d959
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-08 13:50:45 +0300

    Сделал Task4_7

---
+++ Task4_6/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task4_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводится n. Определить, является ли оно степенью 2-ки?
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
