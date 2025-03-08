3acc1bed7d2660541e0e7265e5816522ca5e02f7
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-08 14:11:29 +0300

    fix

--- Task4_6/Program.cs
+++ Task4_6/Program.cs
@@ -5,7 +5,18 @@
         static void Main(string[] args)
         {
             //Вводится n. Определить, является ли оно степенью 2-ки?
-            Console.WriteLine("Hello, World!");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int x = n;
+            int i = 2;
+
+            while (x == 2) ;
+            {
+                x = x / 2;
+                i = ++i;
+                Console.WriteLine(" 2^{0}={1}", i, n);
+            }
+
+            Console.ReadKey();
         }
     }
 }
