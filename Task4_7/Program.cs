36a7d4ca484326951c32742a72563e91e990af26
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-08 13:50:45 +0300

    Сделал Task4_7

---
+++ Task4_7/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task4_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводится n. Определить количество цифр в числе
+            int n = Convert.ToInt32(Console.ReadLine());
+            int x = n;
+            int i = 0;
+
+            do
+            {
+                x = x / 10;
+                i = ++i;
+
+            }
+            while (x > 0);
+            {
+                Console.WriteLine(" количество цифр в числе {0}", i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
