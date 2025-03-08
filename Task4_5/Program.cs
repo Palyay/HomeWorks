189289c619eb41c74ee6cf0226fbe15a19b2a849
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-08 13:15:23 +0300

    Сделал Task4_5

---
+++ Task4_5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            while
+                (a < 20 || a > 60)
+            {
+                Console.WriteLine("введите число из диапазона [20; 60]");
+                Console.ReadKey();
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
