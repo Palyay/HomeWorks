1017225420c14f7ada62c416673500803e5da819
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-16 18:29:37 +0300

    Сделал Task5_4

---
+++ Task5_4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task5_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Сформировать одномерный массив из 10 случайных чисел из диапазона [0, 10]. Перевернуть массив, т.е. переставить элементы массива в обратном порядке
+            const int n = 10;
+            int[] array = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(0, 11);
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    int t = array[i];
+                    array[i] = array[j];
+                    array[j] = t;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
