bb5818cb26ca96b2da2898d30c23ab08f72365f4
Sergey Poliaev <poliaevsa@outlook.com>
2025-03-19 20:43:09 +0300

    fix

--- Task5_7/Program.cs
+++ Task5_7/Program.cs
@@ -7,18 +7,22 @@
             //Сформировать двумерный массив из 10 строк и 5 столбцов. Заполнить его случайными числами в диапазоне [0, 10]. Определить максимальный элемент в каждой строке
             const int n = 10;
             const int m = 5;
-            int[,] array = new int[n, m];
+                        int[,] array = new int[n, m];
             Random random = new Random();
             for (int i = 0; i < n; i++)
             {
-                double s = 0;
+                int a = 0;
+                int b = 0;
+                int max = 0;
                 for (int j = 0; j < m; j++)
                 {
                     array[i, j] = random.Next(0, 11);
-                    s += array[i, j];
+                    a = array[i, j];
+                    b = array[i++, j];
+                    max = (a > b) ? max = a : max=b;
                     Console.Write("{0,4}", array[i, j]);
                 }
-                Console.WriteLine("{0,6:f2}", s / m);
+                Console.WriteLine("{0,6}", max / m);
             }
             Console.ReadKey();
         }
