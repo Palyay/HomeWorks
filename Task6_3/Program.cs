bf051df057ee3b048e359bf39b2d807626aca8a8
Sergey Poliaev <poliaevsa@outlook.com>
2025-04-12 20:09:52 +0300

    Сделал Task6_3

--- Task6_3/Program.cs
+++ Task6_3/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Collections.Generic;
+using System.Text;
 
 namespace Task6_3
 {
@@ -7,32 +7,37 @@ namespace Task6_3
     {
         static void Main(string[] args)
         {
+            //Отчет
             Console.Write("Введите месяц: ");
             string m = Console.ReadLine();
-            m = String.Format("Отчёт о продажах за {0} ", m);
+            m = string.Format("Отчёт о продажах за {0} ", m);
             Console.Write("Введите год: ");
-            string g = Console.ReadLine();
-            g = String.Format("{0,4:0000}", g);
+            int g = Convert.ToInt32(Console.ReadLine());
+            string g1 = string.Format("{0}", g);
             Console.Write("Введите общую сумму продаж: ");
-            string s = Console.ReadLine();
-            string ru_s = s.ToString(CultureInfo.GetCultureInfo("ru-RU"));
-            s = String.Format("Общая сумма продаж: {0:,.F2} р.", ru_s);
+            //  здесь заменить точку на запятую в разделителе я так и не смог,
+            //  в данном типе разрешает только запятую, а если задавать строкой,
+            //  то не вычислить результат
+            double s = Convert.ToDouble(Console.ReadLine());
+            string s1 = string.Format("{0,12:N}", s);
             Console.Write("Введите количество проданных товаров: ");
-            string k = Console.ReadLine();
-            k = String.Format("Количество проданных товаров: {0} шт.", k);
-            Console.Write("Введите cреднюю стоимость товара: ");
-            string sr = Console.ReadLine();
-            sr = String.Format("Средняя стоимость товара: {0:,.F2} р.", sr);
-
+            int k = Convert.ToInt32(Console.ReadLine());
+            string k1 = string.Format("{0,5}", k);
+            //Вычислим cреднюю стоимость товара
+            double sr = s / k;
+            string sr1 = string.Format("{0,8:N}", sr);
+            //Выведем сам отчет
             string razdel = new string('-', 20);
             Console.WriteLine(razdel);
-            Console.WriteLine("{0}{1}", m, g);
+            Console.WriteLine("{0}{1}", m, g1);
             Console.WriteLine(razdel);
-            Console.WriteLine(s);
-            Console.WriteLine(k);
-            Console.WriteLine(sr);
+            Console.WriteLine("Общая сумма продаж: {0} р.", s1);
+            Console.WriteLine("Количество проданных товаров: {0} шт.", k1);
+            Console.WriteLine("Средняя стоимость товара: {0} р.", sr1);
+            Console.WriteLine(razdel);
             Console.ReadKey();
 
         }
     }
 }
+
