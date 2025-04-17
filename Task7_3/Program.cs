2de44b8fbb01bc5239d1bf9af696d6c173319ca6
Sergey Poliaev <poliaevsa@outlook.com>
2025-04-17 21:37:34 +0300

    Task7_3

--- Task7_3/Program.cs
+++ Task7_3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.Numerics;
+using System.Reflection.Metadata;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Task7_3
@@ -9,41 +10,40 @@ namespace Task7_3
     {
         static void Main()
         {
-            // Используется значение по умолчаниюдля массива
-            PrintNumbers(numbers);
-            // Передается массив в обратном порядке
-            PrintNumbers(numbers, true);
-
-            const int n = 10;
-            int[] numbers = new int[n];
-            int[] reversNumbers = new int[n];
-            Random rnd = new Random();
-            for (int i = 0; i < n; i++)
+            Console.WriteLine("введите false или true: ");
+            bool reverse = Convert.ToBoolean(Console.ReadLine());
+            // Пример вызова:
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            Console.Write("Вывод: ");
+            PrintNumbers(numbers, reverse);
+            Console.ReadKey();
+        }
+        static void PrintNumbers(int[] numbers, bool reverse)
+        {
+            if (reverse == false)
             {
-                numbers[i] = rnd.Next(0, n + 1);
-                Console.Write("{0} ", numbers[i]);
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write("{0} ", numbers[i]);                    
+                }
             }
-            Console.WriteLine();
-            for (int i = 0; i < n / 2; i++)
+            if (reverse == true)
             {
-                int t = reversNumbers[i];
-                reversNumbers[i] = reversNumbers[n - i - 1];
-                reversNumbers[n - i - 1] = t;
+                for (int i = 0; i < 5 / 2; i++)
+                {
+                    int t = numbers[i];
+                    numbers[i] = numbers[5 - i - 1];
+                    numbers[5 - i - 1] = t;
+                }
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write("{0} ", numbers[i]);                   
+                }
             }
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write("{0} ", reversNumbers[i]);
-            }
-
-            //  Метод для вывода массива
-            static void PrintNumbers(string[] reversNumbers, string[] numbers);
-            {
-                Console.WriteLine($"{numbers}, {reversNumbers}");
-            }
-                        Console.ReadKey();
         }
     }
 }
+
 
 
 
