6e632511014582d7c4c14f012f95ca5529ac436d
Sergey Poliaev <poliaevsa@outlook.com>
2025-04-17 21:51:52 +0300

    fix

---
+++ Task7_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Task7_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пример вызова:
+            int maxNumber = FindMax(10, 25, 5, 30, 15);
+            Console.WriteLine($"Max number: {maxNumber}"); // Вывод: Max number: 30
+        }
+        // Метод который возвращает максимальное значение
+        static int FindMax(params int[] numbers)
+        {
+            foreach (int a in numbers)
+            {
+                int max = 0;
+                if (a > max)
+                {
+                    max = a;
+
+                }
+            }
+        }
+    }
+}
