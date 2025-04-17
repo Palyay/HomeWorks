5f8a95a1dc1c7bec25c4faacd2197f8c34279479
Sergey Poliaev <poliaevsa@outlook.com>
2025-04-17 22:01:13 +0300

    сделал Task7_4

--- Task7_4/Program.cs
+++ Task7_4/Program.cs
@@ -8,20 +8,19 @@ namespace Task7_4
         {
             // Пример вызова:
             int maxNumber = FindMax(10, 25, 5, 30, 15);
-            Console.WriteLine($"Max number: {maxNumber}"); // Вывод: Max number: 30
+            Console.WriteLine($"Max number: {maxNumber}");// Вывод: Max number
+            int maxNumber1 = FindMax(10, 25, 5, 30, 15, 25, 48);
+            Console.WriteLine($"Max number1: {maxNumber1}"); // Вывод: Max number1
         }
         // Метод который возвращает максимальное значение
         static int FindMax(params int[] numbers)
         {
-            foreach (int a in numbers)
+            int max = 0;
+            foreach (int n in numbers)
             {
-                int max = 0;
-                if (a > max)
-                {
-                    max = a;
-
-                }
+                max = n > max ? n : max;
             }
+            return max;
         }
     }
 }
