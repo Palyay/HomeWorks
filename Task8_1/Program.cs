7d862d00cd00252ff1fdfcddb33829248a73d5c9
Sergey Poliaev <poliaevsa@outlook.com>
2025-04-19 19:16:06 +0300

    fix

--- Task8_1/Program.cs
+++ Task8_1/Program.cs
@@ -1,23 +1,61 @@
-﻿namespace Task8_1
+﻿using System.Linq.Expressions;
+using System.Reflection.Metadata;
+
+namespace Task8_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
             try
             {
-                Console.WriteLine("Начало оперции!");
-                int d = a / b;
-                Console.WriteLine(d);
+                Console.WriteLine("Введите два целых числа");
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите код оперции (1,2,3 или 4)");
+                int c = Convert.ToInt32(Console.ReadLine());
+                if (c < 1 || c > 4)
+                {
+                    Console.WriteLine("Ошибка оперции - неверный код оперции");
+                    Console.ReadKey();
+                }
+                int s, r, p, d;
+                int itog = CalculatePro(a, b, c);
+                Console.WriteLine($"итог: {itog}");
             }
-            catch (Exception e)
+            catch (FormatException)
             {
-                Console.WriteLine(e.ToString());
+                Console.WriteLine("Ошибка оперции - введено не числовое значение");
             }
+            Console.WriteLine("начало оперции");
+        }
 
-            Console.ReadKey();
+        static int CalculatePro(int a, int b, int c)
+        {
+            if (c == 1)
+            {
+                return a + b;
+            }
+            if (c == 2)
+            {
+                return a - b;
+            }
+            if (c == 3)
+            {
+                return a * b;
+            }
+            try
+            {
+                if (c == 4)
+                {
+                    return a / b;
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Ошибка оперции - деление на ноль");
+            }
         }
     }
 }
+
