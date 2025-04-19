2d239d7cfd30000f9fe57e2878f6e8be8a063d81
Sergey Poliaev <poliaevsa@outlook.com>
2025-04-19 11:58:23 +0300

    fix

---
+++ Task8_1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task8_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a/b);
+            Console.ReadKey();
+        }
+    }
+}
