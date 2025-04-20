c0593f26d73f134800328352e2bd0a07174867c2
Sergey Poliaev <poliaevsa@outlook.com>
2025-04-20 20:40:51 +0300

    fix

--- Task9_1/Program.cs
+++ Task9_1/Program.cs
@@ -15,6 +15,38 @@
             //   Инициализирует все поля и свойства
             //   Метод:
             //   GetInfo() – возвращает строку с информацией о книге(например: "Война и мир, Л. Толстой, 1869, 1225 стр.")
+
+            Cat cat = new Cat();
+            Cat cat1 = new Cat();
+            Cat cat2 = new Cat();
+
+
+
+
+
+        }
+    }
+    class Cat
+    {
+        string name;
+        string color;
+        int age;
+
+        //public Cat(string n, string c, int a)
+        //{
+        //    name = n;
+        //    color = c;
+        //    age = a;
+        //}Cat(string n)
+        //{
+        //    name = n;
+        //    color = "Мульти";
+        //    age = 1;
+        //}
+
+        void Sey()
+        {
+            Console.WriteLine("Мяу");
         }
     }
 }
