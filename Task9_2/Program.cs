9250956824009de19533600816f71b099487e2da
Sergey Poliaev <poliaevsa@outlook.com>
2025-04-25 23:09:48 +0300

    fix

--- Task9_2/Program.cs
+++ Task9_2/Program.cs
@@ -1,50 +1,77 @@
-﻿namespace Task9_2
+﻿using System;
+
+namespace Task9_2
 {
     internal class Program
     {
         static void Main(string[] args)
         //        Создайте класс BankAccount, который моделирует банковский счет:
-        //Поля:
-        //    _balance(текущий баланс, private)
-        //Статическое поле:
-        //    _totalAccounts(общее количество созданных счетов, private)
-        //Автосвойства:
-        //    AccountNumber(номер счета, readonly)
-        //    Balance(public геттер, private сеттер)
-        //Конструктор:
-        //    Увеличивает _totalAccounts и генерирует AccountNumber(например, случайный 4-значный номер).
-        //Методы:
-        //    Deposit(decimal amount) – пополняет баланс.
-        //    Withdraw(decimal amount) – снимает деньги(если хватает средств, иначе выбрасывает исключение).
-        //    ShowTotalAccounts() - выводит общее количество созданных счетов.
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount cat = new BankAccount();
+            BankAccount cat1 = new BankAccount();
+            BankAccount cat2 = new BankAccount();
+            cat.Deposit();
+            cat1.Deposit();
+            cat.Deposit();
+            cat.Deposit();
+            cat2.Deposit();
+            cat2.Withdraw();
+            cat1.Withdraw();
+            cat2.Withdraw();
+            cat2.Withdraw();
+            BankAccount.ShowTotalAccounts();
+            Console.ReadKey();
         }
     }
     class BankAccount
     {
-        static decimal balance = 0;
-        decimal totalAccounts = 0;
-        public decimal AccountNumber { get; } = 0;
-        public decimal Balance { get; private set; }        
-        public BankAccount(decimal totalAccounts, decimal accountNumber)
+        decimal _balance = 1000.25M;
+        private static int _totalAccounts = 0;
+        public int[] AccountNumber { get; }
+        public static decimal Balance { get; private set; }
+        public BankAccount()
         {
-            this.totalAccounts = totalAccounts++;
-            this.AccountNumber = accountNumber;
+            Balance = _balance;
+            int[] numbers = new int[4];
+            Random random = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                numbers[i] = random.Next(0, 10);
+                AccountNumber[i] = numbers[i];
+            }
+            _totalAccounts++;
         }
         public void Deposit()
-        {            
-            Console.WriteLine("Сумма для пополнения балонса");
-            decimal sumPopoln = 500;
-            balance = balance + sumPopoln;
-            Console.WriteLine($"Баланс пополнен на {sumPopoln}");
+        {
+            Console.WriteLine($"Счет: {AccountNumber}, Сумма для пополнения балонса: ");
+            decimal plus = Convert.ToDecimal(Console.ReadLine());
+            Balance = _balance + plus;
+            _balance = Balance;
         }
         public void Withdraw()
         {
-            Console.WriteLine("Сумма для снятия");
-            decimal sumSnim = 380;
-            balance = balance - sumSnim;
-            Console.WriteLine($" C баланса списано {sumSnim}");
+            Console.WriteLine($"Счет: {AccountNumber}, Сумма для снятия: ");
+            decimal minus = Convert.ToDecimal(Console.ReadLine());
+            try
+            {
+                Balance = _balance - minus;
+                if (Balance < 0)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    _balance = Balance;
+                }
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Ошибка: недостаточно средств: ");
+            }
+        }
+        public static void ShowTotalAccounts()
+        {
+            Console.WriteLine($"Всего созданных счетов: {_totalAccounts}");
         }
     }
 }
