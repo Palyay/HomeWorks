166136c0641f6f8a7711b88d5f9b3f07b3959c13
Sergey Poliaev <poliaevsa@outlook.com>
2025-05-03 14:30:18 +0300

    fix

--- Task10_1/Program.cs
+++ Task10_1/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Building triangle = new Building();
+
+            Console.ReadKey();
+        }
+    }
+    class Building
+    {
+        string _address;
+        double _area;
+        int yearBuilt;
+        public double BuildingAge { get; set; }
+
+        public virtual double CalculateTax()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+        public virtual double DisplayInfo()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+    class MultiBuilding() : Building
+    {
+        MultiBuilding()
+        : base()
+        {
+
+            int _floors;
+            bool _hasElevator;
         }
     }
 }
+
