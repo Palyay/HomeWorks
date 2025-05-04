4d775a4e3412a0f276af3691a1094f451e4cbd78
Sergey Poliaev <poliaevsa@outlook.com>
2025-05-04 17:18:34 +0300

    fix

--- Task10_1/Program.cs
+++ Task10_1/Program.cs
@@ -7,7 +7,14 @@ namespace Task10_1
     {
         static void Main(string[] args)
         {
-            Building triangle = new Building("Сливовая",2,3);
+            Building triangle = new Building("Сливовая", 190.15, 1857);
+            MultiBuilding triangle1 = new MultiBuilding("Каштановая", 2851.52, 2003, 9, true);
+            Building triangle2 = triangle;
+            MultiBuilding triangle3 = (MultiBuilding)triangle;
+            triangle2.CalculateTax();
+            triangle2.DisplayInfo();
+            triangle3.CalculateTax();
+            triangle3.DisplayInfo();
             Console.ReadKey();
         }
     }
@@ -23,6 +30,7 @@ namespace Task10_1
             _address = ad;
             _area = ar;
             _yearBuilt = ye;
+            BuildingAge = 2015 - _yearBuilt;
         }
         public virtual void CalculateTax()  // рассчитывает налог (базовая формула: площадь × 1000)
         {
@@ -43,21 +51,26 @@ namespace Task10_1
         public MultiBuilding(string ad, double ar, int ye, int fl, bool Elevator)
              : base(ad, ar, ye)
         {
-            _address = ad;
-            _area = ar;
-            _yearBuilt = ye;
             _floors = fl;
             _hasElevator = Elevator;
             AreaPerFloor = _area / _floors;
         }
         public override void CalculateTax()  // рассчитывает налог (1 + (_floors - 1) * 0.05)
         {
-            double n = _area * 1000 * (1 + (_floors - 1) * 0.05);
+            double n;
+            if (_hasElevator == true)
+            {
+                n = _area * 1000 * (1 + (_floors - 1) * 0.05) + 5000;
+            }
+            else
+            {
+                n = _area * 1000 * (1 + (_floors - 1) * 0.05);
+            }
             Console.WriteLine($"налог: {n}");
         }
         public override void DisplayInfo()  //выводит информацию о многоэтажном здании
         {
-            Console.WriteLine($"адрес здания : {_address}, площадь в квадратных метрах - {_area} , год постройки - {_yearBuilt}, количество этажей - {_floors}, наличие лифта - {_hasElevator}");
+            Console.WriteLine($"адрес здания : {_address}, площадь в квадратных метрах - {_area} , год постройки - {_yearBuilt}, количество этажей - {_floors}, средняя площадь на этаж - {AreaPerFloor}, наличие лифта - {_hasElevator}");
         }
     }
 }
