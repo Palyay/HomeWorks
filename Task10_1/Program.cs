058c20a0fa1132ab7cf3d113bcef57dc2b0c3a53
Sergey Poliaev <poliaevsa@outlook.com>
2025-05-04 16:23:26 +0300

    fix

--- Task10_1/Program.cs
+++ Task10_1/Program.cs
@@ -1,40 +1,65 @@
-﻿namespace Task10_1
+﻿using System;
+using System.Net;
+
+namespace Task10_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Building triangle = new Building();
+            Building triangle = new Building("Сливовая",2,3);
             Console.ReadKey();
         }
     }
     class Building
     {
-        string _address;
-        double _area;
-        int _yearBuilt;
+        protected string _address;
+        protected double _area;
+        protected int _yearBuilt;
         public double BuildingAge { get; set; }
-
-        public virtual void CalculateTax()
+        //  Конструктор базового класса
+        public Building(string ad, double ar, int ye)
         {
-            double p = _area * 1000;
-            Console.WriteLine($"налог: {p}");
+            _address = ad;
+            _area = ar;
+            _yearBuilt = ye;
         }
-        public virtual void DisplayInfo()
+        public virtual void CalculateTax()  // рассчитывает налог (базовая формула: площадь × 1000)
         {
-            Console.WriteLine($"адрес здания {_address}, площадь в квадратных метрах {_area} , год постройки {_yearBuilt} "); 
+            double n = _area * 1000;
+            Console.WriteLine($"налог: {n}");
+        }
+        public virtual void DisplayInfo()  //выводит информацию о здании
+        {
+            Console.WriteLine($"адрес здания {_address}, площадь в квадратных метрах {_area} , год постройки {_yearBuilt} ");
         }
     }
-    class MultiBuilding() : Building
+    sealed class MultiBuilding : Building
     {
-        MultiBuilding()
-        : base()
+        int _floors;
+        bool _hasElevator;
+        public double AreaPerFloor { get; }
+        //  Конструктор класса-наследника
+        public MultiBuilding(string ad, double ar, int ye, int fl, bool Elevator)
+             : base(ad, ar, ye)
         {
-            int _floors;
-            bool _hasElevator;
-            public double AreaPerFloor { get;}
-
+            _address = ad;
+            _area = ar;
+            _yearBuilt = ye;
+            _floors = fl;
+            _hasElevator = Elevator;
+            AreaPerFloor = _area / _floors;
+        }
+        public override void CalculateTax()  // рассчитывает налог (1 + (_floors - 1) * 0.05)
+        {
+            double n = _area * 1000 * (1 + (_floors - 1) * 0.05);
+            Console.WriteLine($"налог: {n}");
+        }
+        public override void DisplayInfo()  //выводит информацию о многоэтажном здании
+        {
+            Console.WriteLine($"адрес здания : {_address}, площадь в квадратных метрах - {_area} , год постройки - {_yearBuilt}, количество этажей - {_floors}, наличие лифта - {_hasElevator}");
         }
     }
 }
+
 
