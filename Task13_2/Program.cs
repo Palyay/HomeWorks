b05f83d08097073d635f7b564f5f5085bb049ddb
Sergey Poliaev <poliaevsa@outlook.com>
2025-05-25 21:54:30 +0300

    fix

---
+++ Task13_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Channels;
+
+namespace Task13_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public delegate void MyDelegate(string message);
+
+    public class SmartHomeSystem(string Light, string Thermostat, string Door)
+    {
+        public event MyDelegate MyDelegate;
+        string Light = "Light";
+        string Thermostat = "Thermostat";
+        string Door = "Door";
+
+        public void TurnOnLight();
+        
+
+}
+}
+
+
