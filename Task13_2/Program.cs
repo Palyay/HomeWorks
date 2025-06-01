96a0fe757cae0f56469d4641b529ad1453d45b38
Sergey Poliaev <poliaevsa@outlook.com>
2025-06-01 12:51:00 +0300

    Сделал Task13_2

--- Task13_2/Program.cs
+++ Task13_2/Program.cs
@@ -6,22 +6,52 @@ namespace Task13_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //создаем экземпляр класса умного дома
+            SmartHomeSystem alex = new SmartHomeSystem("0", "0", "0");
+            // Подписываемся на событие изменения состояния любого устройства
+            alex.DeviceStatus += (Device, message) =>
+            {
+                Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {Device} {message}");
+            };
+            //Проверим работу устройства "алекс"
+            {
+                alex.TurnOffLight();
+                alex.TurnOnLight();
+                alex.SetTemperature(23);
+                alex.UnlockDoor();
+                alex.LockDoor();
+            }
+        }
+        public delegate void DeviceStateChanged(string Device, string message);
+
+        public class SmartHomeSystem(string Light, string Thermostat, string Door)
+        {
+            public event DeviceStateChanged DeviceStatus;
+            string Light = "Light:";
+            string Thermostat = "Thermostat:";
+            string Door = "Door:";
+            public void TurnOnLight()
+            {
+                DeviceStatus?.Invoke(Light, "Выключен");
+            }
+            public void TurnOffLight()
+            {
+                DeviceStatus?.Invoke(Light, "Включен");
+            }
+            public void SetTemperature(int newTemp)
+            {
+                DeviceStatus?.Invoke(Thermostat, $"Температура изменена на {newTemp} °C");
+            }
+            public void LockDoor()
+            {
+                DeviceStatus?.Invoke(Door, "Заблокирована");
+            }
+            public void UnlockDoor()
+            {
+                DeviceStatus?.Invoke(Door, "Открыта");
+            }
         }
     }
-    public delegate void MyDelegate(string message);
-
-    public class SmartHomeSystem(string Light, string Thermostat, string Door)
-    {
-        public event MyDelegate MyDelegate;
-        string Light = "Light";
-        string Thermostat = "Thermostat";
-        string Door = "Door";
-
-        public void TurnOnLight();
-        
-
-}
 }
 
 
