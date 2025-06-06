96aca542a7c6e41816775a64e77163c0e5eafa78
Sergey Poliaev <poliaevsa@outlook.com>
2025-06-06 21:23:28 +0300

    Сделал Task14_1

--- Task14_1/Program.cs
+++ Task14_1/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> values = new List<string>();
+            values.Add("Иванов");
+            values.Add("Петров");
+            values.Add("Сидоров");
+
+            values.Insert(1, "Козлов");
+            Console.WriteLine();
+
+            bool player = values.Contains("Петров");
+            Console.WriteLine($"Есть ли игрок 'Петров' в команде? {player}");
+
+            values.Remove("Сидоров");
+
+            int ind1 = values.IndexOf("Козлов");
+            Console.WriteLine($"Индекс игрока 'Козлов':{ind1}");
+
+            values.Sort();
+
+            Console.WriteLine("Текущий состав команды:");
+            for (int i = 0; i < values.Count; i++)
+            {
+                Console.WriteLine($"{i} {values[i]}");
+            }
+
+            int players = values.Count;
+            string count = players > 0 ? "False" : "True";
+            {
+                Console.WriteLine($"Команда пуста? {count}");
+            }
+
+            values.Clear();
+            Console.WriteLine($"Количество игроков после очистки: {values.Count}");
+
+            Console.ReadKey();
         }
     }
 }
