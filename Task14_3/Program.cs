24bf2b446ff1193b8d9412f7cdd51c273d9ee050
Sergey Poliaev <poliaevsa@outlook.com>
2025-06-08 16:01:48 +0300

    fix

---
+++ Task14_3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task14_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet < string > email = new HashSet<string>();
+            email.Add("alice@example.com");
+            email.Add("bob@example.com");
+            email.Add("charlie@example.com");
+
+           
+            Console.ReadKey();
+        }
+    }
+}
